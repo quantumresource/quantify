730b023618011c33f3fd8cf846e58d11f447fbfc
alexandrupaler <alexandrupaler@gmail.com>
2020-08-06 15:56:29 -0700

    experiment example

--- examples/multiplier_example.py
+++ examples/multiplier_example.py
@@ -5,44 +5,64 @@ from utils.counting_utils import *
 import optimizers as qopt
 import utils.misc_utils as miscutils
 
-nr_bits = 4
+import time
+import sys
 
-A = [cirq.NamedQubit('A'+str(i)) for i in range(nr_bits)]
-B = [cirq.NamedQubit('B'+str(i)) for i in range(nr_bits)]
-control = cirq.NamedQubit('ctrl')
+def main():
 
-multiplier = QimMultiplier(A, B).multiply()
-# print(multiplier.moments)
+      print(f"$$$ START {time.strftime('%H:%M:%S')}")
 
-decomp_mult = cirq.Circuit(ToffoliDecomposition.construct_decomposed_moments(multiplier.moments, ToffoliDecompType.FOUR_ANCILLA_TDEPTH_1_A))
-# print(decomp_mult)
-# print(decomp_mult)
+      for i in range(3, 8):
+            print(f"2^{i} qubits")
 
-print("Before Optimization: ",
-      "depth:", len(decomp_mult),
-      "t-depth:", count_t_depth_of_circuit(decomp_mult),
-      "cnots:", count_cnot_of_circuit(decomp_mult),
-      "h:", count_h_of_circuit(decomp_mult),
-      "t-count:", count_t_of_circuit(decomp_mult))
+            for j in range(10):
+                  print(f"run {j}")
+
+                  nr_bits = 2 ** i
+
+                  A = [cirq.NamedQubit('A'+str(i)) for i in range(nr_bits)]
+                  B = [cirq.NamedQubit('B'+str(i)) for i in range(nr_bits)]
+                  # control = cirq.NamedQubit('ctrl')
+
+                  multiplier = QimMultiplier(A, B).multiply()
+                  # print(multiplier.moments)
+
+                  decomp_mult = cirq.Circuit(ToffoliDecomposition.construct_decomposed_moments(multiplier.moments, ToffoliDecompType.FOUR_ANCILLA_TDEPTH_1_A))
+
+                  # print("Before Optimization: ",
+                  #       "depth:", len(decomp_mult),
+                  #       "t-depth:", count_t_depth_of_circuit(decomp_mult),
+                  #       "cnots:", count_cnot_of_circuit(decomp_mult),
+                  #       "h:", count_h_of_circuit(decomp_mult),
+                  #       "t-count:", count_t_of_circuit(decomp_mult))
 
 
-# miscutils.flag_operations(decomp_mult, [cirq.ops.H])
-qopt.CancelNghHadamards(transfer_flag=False).optimize_circuit(decomp_mult)
-qopt.CancelNghCNOTs(transfer_flag=False) \
-            .apply_until_nothing_changes(decomp_mult, count_cnot_of_circuit)
+                  start2 = time.time()
+
+                  # miscutils.flag_operations(decomp_mult, [cirq.ops.H])
+                  qopt.CancelNghHadamards(transfer_flag=False).optimize_circuit(decomp_mult)
+                  qopt.CancelNghCNOTs(transfer_flag=False) \
+                              .apply_until_nothing_changes(decomp_mult, count_cnot_of_circuit)
 
 
-qopt.CommuteTGatesToStart().optimize_circuit(decomp_mult)
-qopt.CommuteTGatesToStart().optimize_circuit(decomp_mult)
-qopt.ParallelizeCNOTSToLeft().optimize_circuit(decomp_mult)
+                  qopt.CommuteTGatesToStart().optimize_circuit(decomp_mult)
+                  qopt.CommuteTGatesToStart().optimize_circuit(decomp_mult)
+                  qopt.ParallelizeCNOTSToLeft().optimize_circuit(decomp_mult)
 
-cirq.optimizers.DropEmptyMoments().optimize_circuit(decomp_mult)
+                  cirq.optimizers.DropEmptyMoments().optimize_circuit(decomp_mult)
 
-# print(decomp_mult)
+                  stop2 = time.time() - start2
+                  print(f"$$$ {time.strftime('%H:%M:%S')} stop: {stop2}")
+                  sys.stdout.flush()
 
-print("After Optimization:  ",
-      "depth:", len(decomp_mult),
-      "t-depth:", count_t_depth_of_circuit(decomp_mult),
-      "cnots:", count_cnot_of_circuit(decomp_mult),
-      "h:", count_h_of_circuit(decomp_mult),
-      "t-count:", count_t_of_circuit(decomp_mult))
+            # print(decomp_mult)
+
+            # print("After Optimization:  ",
+            #       "depth:", len(decomp_mult),
+            #       "t-depth:", count_t_depth_of_circuit(decomp_mult),
+            #       "cnots:", count_cnot_of_circuit(decomp_mult),
+            #       "h:", count_h_of_circuit(decomp_mult),
+            #       "t-count:", count_t_of_circuit(decomp_mult))
+
+if __name__ == "__main__":
+    main()
