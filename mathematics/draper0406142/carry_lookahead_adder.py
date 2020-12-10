160268a75ff413ef192445b0895fc8782a52c6cb
oumjunior <oumar1195@gmail.com>
2020-12-10 13:08:01 +0100

    doc

--- mathematics/draper0406142/carry_lookahead_adder.py
+++ mathematics/draper0406142/carry_lookahead_adder.py
@@ -1,17 +1,29 @@
 # TODO: Document the code !!
-
+"""
+  Implementation of the carry lookahead adder 
+  arxiv preprint 0406142
+"""
 import cirq
 import math as mt
 from qramcircuits.toffoli_decomposition import ToffoliDecomposition, ToffoliDecompType
 
 class CarryLookaheadAdder:
   def __init__(self, A, B, decompositon_strategy = [ToffoliDecompType.NO_DECOMP]*2):
+    """
+      params: A: quantum register holding the first integer operand
+      params: B: quantum register holding the second integer operand
+      params: decompositon_strategy: Toffoli decomposition to use in the p-round and the other rounds
+
+    """
     self.A, self.B = A, B
     self.rounds = self.construct_rounds()
     self.decompositon_strategy = decompositon_strategy
     self.circuit = self.construct_circuit()
     
   def construct_rounds(self):
+    """
+      return the init, p-round, g-round and the c-round of the carry lookahead adder
+    """
     n = len(self.A)
     init = []
     p_round = []
@@ -22,16 +34,8 @@ class CarryLookaheadAdder:
     l2 = n-w-l
     ancillae1 = [cirq.NamedQubit("a"+str(i)) for i in range(n)]
     ancillae2 = [cirq.NamedQubit("b"+str(i)) for i in range(l2)]
-    # ancillae2 = {}
-    # ancillae2[-1] = [cirq.NamedQubit(str(k)+str(i)) for i in range(n)]
-    # for k in range(l-1):
-    #   start = 2**k
-    #   step = 2**(k+1)
-    #   u = (n-start)//step
-    #   ancillae2[k] = [cirq.NamedQubit(str(k)+str(i)) for i in range(u)]
-
-    # Init
-
+    
+    # Init round
     for i in range(n):
       init.append(cirq.TOFFOLI(self.A[i], self.B[i], ancillae1[i]))
       init.append(cirq.CNOT(self.A[i], self.B[i]))
@@ -40,14 +44,10 @@ class CarryLookaheadAdder:
     # First moment
     for i in range(2,n-1, 2):
       p_round.append(cirq.TOFFOLI(self.B[i], self.B[i+1], ancillae2[i//2-1]))
-    # Rest of the moments
-    # m = mt.floor(mt.log2(len(ancillae2)))
-    # l2 = (n-2)//2
-    # l3 = mt.floor(mt.log2(l2))
     start = 0
     index = (n-2)
     end1 = (n-2)//2 
-    print(index, end1)
+    # print(index, end1)
     for i in range(1, l-1):
       index = index//2
       start += index
@@ -55,46 +55,33 @@ class CarryLookaheadAdder:
       if end > l2:
         end = l2
       s = 0
-      print(index, start, end)
+      # print(index, start, end)
       k = 1
       for j in range(start,end):
         i1 = j-end1+k
         i2 = j-end1+k+1
-        print(j, i1, i2)
+        # print(j, i1, i2)
         p_round.append(cirq.TOFFOLI(ancillae2[i1], ancillae2[i2], ancillae2[j]))
         s+=1
         k += 1
       end1 -= s
 
-    # G-rounds
+    # G-round
     # First moment
     for i in range(1, n, 2):
       g_round.append(cirq.TOFFOLI(ancillae1[i-1], self.B[i], ancillae1[i]))
 
-    # start = 0
-    # index = (n-2)
-    # end1 = (n-2)//2 
-    # print(index, end1)
     start = 0
-    # range = n
     k = 0
     for i in range(l-1):
       index = 2**i
       start += index
-      # range = range//2
-      # end = start + range 
-      # if end > l2:
-      #   end = l2
-      # s = 0
-      # saut = (n-2)//2
       
       step = 2**(i+2)
       end = start+(n-start)//step*(step)+1
-      # if end == 0:
-      #   end = start+1
-      print("###################################")
-      print(index, start, end)
-      print("###################################")
+      # print("###################################")
+      # print(index, start, end)
+      # print("###################################")
       k_anc = k
       for j in range(start,end, step):
         i1 = j
@@ -103,15 +90,13 @@ class CarryLookaheadAdder:
         if target > n:
           pass
         else:
-          print(i1, i2, target)
+          # print(i1, i2, target)
           g_round.append(cirq.TOFFOLI(ancillae1[i1], ancillae2[i2], ancillae1[target]))
-          # s+=1
           k += 2
       k = k_anc + (n-2)//(2**(i+1))
 
-    ######## C-rounds
-    # First moment
-
+    # C-round
+    # First moment of C-round
     for i in range(1, n-1, 2):
       c_round.append(cirq.TOFFOLI(ancillae1[i], self.B[i+1], ancillae1[i+1]))
 
@@ -121,11 +106,11 @@ class CarryLookaheadAdder:
       start = 2**k-1
       step = 2**k
       span = 2**(k-1)
-      print("#############c-round##########")
-      print(start, step, span)
-      print("############################")
+      # print("#############c-round##########")
+      # print(start, step, span)
+      # print("############################")
       for p in range(start, n, step):
-        print(p)
+        # print(p)
         if p+span>n:
           pass
         else:
@@ -142,8 +127,12 @@ class CarryLookaheadAdder:
     
     return init, p_round, g_round, c_round[::-1], last_round
 
+ 
   def construct_circuit(self):
-
+    """
+      returns the CLA circuit
+    """
+    
     """
       Computation part of the circuit
     """
@@ -194,8 +183,8 @@ class CarryLookaheadAdder:
     """
       Uncomputation part of the circuit
     """
-
-    index = (len(self.A)-2)//2
+    n = len(self.A)
+    index = (n-2)//2
 
     p_round_uncom = p_round_comp[:index-1] + p_round_comp[index:]
     circuit += cirq.Circuit(
